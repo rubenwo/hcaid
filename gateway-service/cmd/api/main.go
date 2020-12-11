dd3f8d9d65ef0432566674bdf92ed00b2fc665b0
Ruben <rwoldhui@avans.nl>
2020-12-11 18:16:09 +0100

    [Added] TLS to gateway

--- gateway-service/cmd/api/main.go
+++ gateway-service/cmd/api/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"github.com/rs/cors"
 	"github.com/rubenwo/home-automation/gateway-service/pkg/ingress"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -32,7 +34,33 @@ func main() {
 		AllowCredentials: true,
 	}).Handler(router)
 
-	if err := http.ListenAndServe(":80", handler); err != nil {
+	tlsCfg := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		//CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		//PreferServerCipherSuites: true,
+		//CipherSuites: []uint16{
+		//	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		//	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+		//	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+		//	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		//},
+	}
+
+	server := &http.Server{
+		Addr:         ":443",
+		Handler:      handler,
+		TLSConfig:    tlsCfg,
+		ReadTimeout:  time.Second * 60,
+		WriteTimeout: time.Second * 60,
+		IdleTimeout:  time.Second * 120,
+	}
+	go func() {
+		if err := http.ListenAndServe(":80", handler); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if err := server.ListenAndServeTLS("/certs/fullchain.pem", "/certs/privkey.pem"); err != nil {
 		log.Fatal(err)
 	}
 }
